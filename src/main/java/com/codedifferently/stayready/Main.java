784ffdccce7990b868af9c91c2c13aa488e984a5
Lissette Garcia <lissettegarcia@Lissettes-MacBook-Air.local>
2020-07-09 11:02:17 -0500

    Completed lab05 with Dominic

--- src/main/java/com/codedifferently/stayready/Main.java
+++ src/main/java/com/codedifferently/stayready/Main.java
@@ -1,4 +1,54 @@
 package com.codedifferently.stayready;
 
 public class Main {
+
+    public double dollarToEuro(double dollar) {
+        double euro = dollar *  0.89;
+        return euro;
+    }
+
+    public String euroToDollar(double euro) {
+        double dollar = euro * 1.13;
+        return String.format("%.2f", dollar);
+    }
+
+    public double euroToPound(double euro){
+        double pound = euro * 0.90;
+        return pound;
+    }
+
+    public double poundToRupee(double pound) {
+        double rupee = pound * 94.78;
+        return rupee;
+    }
+
+    public double rupeeToCanadian(double rupee) {
+        double canadian = rupee * .018;
+        return canadian;
+    }
+
+    public double canadianToSingapore(double canadian) {
+        double singapore = canadian * 1.03;
+        return singapore;
+    }
+
+    public double singaporeToFranc(double singapore) {
+        double franc = singapore * 0.67;
+        return franc;
+    }
+
+    public double francToRinggit(double franc) {
+        double ringitt = franc * 4.54;
+        return ringitt;
+    }
+
+    public double ringittToYen(double ringitt) {
+        double yen = ringitt * 25.11;
+        return yen;
+    }
+
+    public double yenToChinese(double yen) {
+        double chinese = yen * 0.0656;
+        return chinese;
+     }
 }
--- src/test/java/com/codedifferently/stayready/MainTest.java
+++ src/test/java/com/codedifferently/stayready/MainTest.java
@@ -1,4 +1,147 @@
 package com.codedifferently.stayready;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MainTest {
+
+    @Test
+    public void dollarToEuroTest() {
+        //Given
+        Main dollar = new Main();
+        double d = 20;
+        double expected = 17.80;
+
+        //When
+        double actual = dollar.dollarToEuro(d);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void euroToDollarTest() {
+        //Given
+        Main euro = new Main();
+        double e = 20;
+        String expected = "22.60";
+
+        //When
+        String actual = euro.euroToDollar(e);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void euroToPoundTest() {
+        //Given
+        Main pound = new Main();
+        double p = 20;
+        double expected = 18.00;
+
+        //When
+        double actual = pound.euroToPound(p);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void poundToRupeeTest() {
+        //Given
+        Main rupee = new Main();
+        double r = 20;
+        double expected = 1895.60;
+
+        //When
+        double actual = rupee.poundToRupee(r);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void rupeeToCanadianTest() {
+        //Given
+        Main canadian = new Main();
+        double c = 20;
+        double expected = .36;
+
+        //When
+        double actual = canadian.rupeeToCanadian(c);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void canadianToSingaporeTest() {
+        //Given
+        Main singapore = new Main();
+        double s = 20;
+        double expected = 20.60;
+
+        //When
+        double actual = singapore.canadianToSingapore(s);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void singaporeToFrancTest() {
+        //Given
+        Main franc = new Main();
+        double f = 20;
+        double expected = 13.40;
+
+        //When
+        double actual = franc.singaporeToFranc(f);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void francToRingittTest() {
+        //Given
+        Main ringitt = new Main();
+        double r = 20;
+        double expected = 90.80;
+
+        //When
+        double actual = ringitt.francToRinggit(r);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void ringittToYenTest() {
+        //Given
+        Main yen = new Main();
+        double y = 20;
+        double expected = 502.2;
+
+        //When
+        double actual = yen.ringittToYen(y);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+
+    @Test
+    public void yenToChineseTest() {
+        //Given
+        Main chinese = new Main();
+        double c = 20;
+        double expected = 1.312;
+
+        //When
+        double actual = chinese.yenToChinese(c);
+
+        //Then
+        Assert.assertEquals(expected, actual, 0.0);
+    }
 }
